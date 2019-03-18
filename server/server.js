f9fc0bb73642d0c7a3e367594b48d5e21f4ebac4
Nezetaka <karen.kaloyan@gmail.com>
2019-03-18 16:37:33 +0200

    sockets on chat

--- server/server.js
+++ server/server.js
@@ -4,16 +4,24 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const nunjucks = require('nunjucks');
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {serveClient: true});
 
-nunjucks.configure('./client', {
+nunjucks.configure('./client/views', {
   autoescape: true,
   express: app
 });
+
+app.use('/assets', express.static('./client/public'));
 
 app.get('/', (req, res) => {
   res.render('index.html');
 });
 
-app.listen(3000, '0.0.0.0', () => {
+io.on('connection', function (socket) {
+  socket.emit('connected', 'You are connected.');
+});
+
+server.listen(3000, '0.0.0.0', () => {
   console.log('Server started on port 3000');
 });
